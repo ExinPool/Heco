a16ae09ef66f7b71608dd3ba86c06916f77eda45
Robin Wen <blockxyz@gmail.com>
2021-09-25 12:35:11 +0800

    Fix bug

--- blocks/heco_blocks.sh
+++ blocks/heco_blocks.sh
@@ -24,16 +24,16 @@ webhook_url="$(config_get WEBHOOK_URL)"
 access_token="$(config_get ACCESS_TOKEN)"
 
 local_blocks_first_hex=`curl -X POST ${local_host} -H "Content-Type: application/json" --data '{"jsonrpc":"2.0", "method":"eth_blockNumber", "params":[], "id":83}' | jq | grep result | sed "s/\"//g" | awk -F':' '{print $2}' | sed "s/ //g" | sed "s/0x//g"`
-local_blocks_first=`echo $((16#${local_blocks_hex}))`
+local_blocks_first=`echo $((16#${local_blocks_first_hex}))`
 sleep ${sleep_number}
 local_blocks_second_hex=`curl -X POST ${local_host} -H "Content-Type: application/json" --data '{"jsonrpc":"2.0", "method":"eth_blockNumber", "params":[], "id":83}' | jq | grep result | sed "s/\"//g" | awk -F':' '{print $2}' | sed "s/ //g" | sed "s/0x//g"`
-local_blocks_second=`echo $((16#${local_blocks_hex}))`
-log="`date '+%Y-%m-%d %H:%M:%S'` UTC `hostname` `whoami` INFO local_blocks: ${local_blocks}, remote_first_blocks: ${remote_first_blocks}, remote_second_blocks: ${remote_second_blocks}"
+local_blocks_second=`echo $((16#${local_blocks_second_hex}))`
+log="`date '+%Y-%m-%d %H:%M:%S'` UTC `hostname` `whoami` INFO local_blocks_first: ${local_blocks_first}, local_blocks_second: ${local_blocks_second}."
 echo $log >> $log_file
 
 if [ ${process_num} -eq ${process_num_var} ] && [ ${local_blocks_first} -eq ${local_blocks_second} ]
 then
-    log="时间: `date '+%Y-%m-%d %H:%M:%S'` UTC \n主机名: `hostname` \n节点: ${local_host}, ${local_blocks} \n状态: 同步区块停滞，已重启节点。"
+    log="时间: `date '+%Y-%m-%d %H:%M:%S'` UTC \n主机名: `hostname` \n节点: ${local_host}, ${local_blocks_first} \n节点: ${local_host}, ${local_blocks_second} \n状态: 同步区块停滞，已重启节点。"
     echo -e $log >> $log_file
     success=`curl ${webhook_url}=${access_token} -XPOST -H 'Content-Type: application/json' -d '{"category":"PLAIN_TEXT","data":"'"$log"'"}' | awk -F',' '{print $1}' | awk -F':' '{print $2}'`
     if [ "$success" = "true" ]
